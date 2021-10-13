c6faf0f7f13bc690714cdec93a0844f0c2c46b55
Pranjal Rawat <pranjal.rawat@QUALITEST.LOCAL>
2021-10-13 10:53:21 +0530

    Added Intermediate Test

--- src/test/java/com/qtest/easy/automation/tests/intermediate/Intermediate.java
+++ src/test/java/com/qtest/easy/automation/tests/intermediate/Intermediate.java
@@ -1,2 +1,17 @@
-package com.qtest.easy.automation.tests.intermediate;public class Intermediate {
+package com.qtest.easy.automation.tests.intermediate;
+
+import com.aventstack.extentreports.Status;
+import com.qtest.automation.TestSessionInitiator;
+import org.testng.annotations.Test;
+
+public class Intermediate extends TestSessionInitiator {
+
+    @Test
+    public void TC_100_verify_intermediate_btn_click(){
+        logger = extent.createTest("TC_100_verify_intermediate_btn_click");
+        intermediateActions.clickOnIntermediate();
+        intermediateActions.verifyActiveTab();
+        logger.log(Status.PASS, "Test Case Passed");
+
+    }
 }
